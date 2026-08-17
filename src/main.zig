f9d8635648a32b6db65ad5f94c3e354269dd1cf2
Daniel Benitez <42282680+pyand3v@users.noreply.github.com>
2026-08-17 09:19:00 +0000

    feat(cli): stream file contents to stdout

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,30 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+    const path = args[1];
+
+    const file = try std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only, .allow_directory = false });
+    defer file.close(io);
+
+    var reader = file.reader(io, &.{}); // [4096]u8
+    var reader_interface = &reader.interface;
+
+    // const content = try reader_interface.allocRemaining(init.arena.allocator(), .limited(1024*1024));
+
+    var stdout_file_writer = std.Io.File.Writer.init(.stdout(), io, &.{});
+    const stdout_writer = &stdout_file_writer.interface; 
+
+    var chunk: [4096]u8 = undefined;
+
+    while (true) {
+        const bytes_read = try reader_interface.readSliceShort(&chunk);
+
+        if (bytes_read == 0) break;
+        
+        try stdout_writer.writeAll(chunk[0..bytes_read]);
+    }
+    try stdout_writer.flush();
 }
+
