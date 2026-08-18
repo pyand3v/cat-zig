81e73625fe70da87c02866323f15e44fd7218512
Daniel Benitez <42282680+pyand3v@users.noreply.github.com>
2026-08-18 05:00:43 +0000

    fix(io): handle reader EOF

--- src/cat.zig
+++ src/cat.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 
 pub fn getFile(io: std.Io, path: [:0]const u8) std.Io.File.OpenError!std.Io.File {
-    const file = std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only, .allow_directory = false }) catch | err | switch (err) {
+    const file = std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only, .allow_directory = false }) catch |err| switch (err) {
         error.FileNotFound => return error.FileNotFound, // TODO: Makes no sense atm, but just as a reminder in case we extend this in the future
         else => return err,
     };
@@ -18,11 +18,15 @@ pub fn copyFileToWriter(io: std.Io, file: std.Io.File, writer_interface: *std.Io
 
 pub fn copyReaderToWriter(reader_interface: *std.Io.Reader, writer_interface: *std.Io.Writer) !void {
     var chunk: [4096]u8 = undefined;
+    var buffers: [1][]u8 = .{&chunk};
 
     while (true) {
-        const bytes_read = try reader_interface.readSliceShort(&chunk);
+        const bytes_read = reader_interface.readVec(&buffers) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
 
-        if (bytes_read == 0) break;
+        if (bytes_read == 0) continue;
 
         try writer_interface.writeAll(chunk[0..bytes_read]);
     }
