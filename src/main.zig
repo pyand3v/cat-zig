a944bd18ef9b8045a4911506387c86bdb56c5d21
Daniel Benitez <42282680+pyand3v@users.noreply.github.com>
2026-08-21 08:25:33 +0000

    feat(cli): support multiple input files

--- src/main.zig
+++ src/main.zig
@@ -4,19 +4,19 @@ const cat = @import("cat.zig");
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const args = try init.minimal.args.toSlice(init.arena.allocator());
-    const path = if (args.len > 1) args[1] else null;
-
+    const paths = args[1..];
 
     var writer = std.Io.File.Writer.init(.stdout(), io, &.{}); // No write buffer.
     const writer_interface = &writer.interface;
 
-    if (path) |file_path| {
+    for (paths) |file_path| {
+        if (std.mem.eql(u8, file_path, "-")) {
+            try cat.copyFileToWriter(io, std.Io.File.stdin(), writer_interface);
+        }
+
         const opened_file = try cat.getFile(io, file_path);
         defer opened_file.close(io);
 
         try cat.copyFileToWriter(io, opened_file, writer_interface);
-    } else {
-        try cat.copyFileToWriter(io, std.Io.File.stdin(), writer_interface);
     }
 }
-
